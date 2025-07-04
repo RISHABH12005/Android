1c3b3b58f17357fbbc79fdc0e58e511ea98734de
DarkZ <146022304+RISHABH12005@users.noreply.github.com>
2025-07-05 02:24:21 +0530

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,13 @@
+git init
+
+git add .
+
+git commit -m "Initial commit of Android"
+
+git remote add origin https://github.com/username/__.git     # '__' Name of the project
+
+git remote -v
+
+git branch -M main
+
+git push -u origin main
